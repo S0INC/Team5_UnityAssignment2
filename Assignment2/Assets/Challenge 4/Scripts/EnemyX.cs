6c31c498989001fb0981826147c3ad3a5298658a
Hamad <72784871+S0INC@users.noreply.github.com>
2025-02-21 21:59:05 +0400

    Finished The Challenge
    completed all challenges and bonus challenges, without any additional changes.

---
+++ Assignment2/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal;
+    public SpawnManagerX spawnManagerX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        // Increases the speed based on the wave number from the SpawnManagerX script
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime * (spawnManagerX.waveCount * 0.4f));
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Team5_UnityAssignment2
  Game Development - Assignment 2
+ Salem Aljaberi 1000
+ Hamad Almutawa 100060822
+ Abdullah almarzooqi 1000
+ 
